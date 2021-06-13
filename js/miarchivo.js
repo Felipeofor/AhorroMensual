2364c7779bdeca4a76a623ffd444b9e095163272
Ramos Felipe <felipeofor@gmail.com>
2021-06-12 22:11:26 -0300

    medio camino

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -8,7 +8,7 @@ function calcular(e) {
 
 
 
-    let ingreso = document.querySelector("#number").value;
+    let ingreso = document.querySelector(".number").value;
 
     let paraTodaLaVida = ingreso * 0.10;
 
@@ -21,56 +21,31 @@ function calcular(e) {
 
     console.log(ingreso, paraTodaLaVida, sueldoReal);
 
-
-    /*
-        console.log(`Tu sueldo es ${ingreso} y vas a ahorra para toda la vida: ${paraTodaLaVida}, con lo que queda podes gastar en: Gastos Basicos:${gastosBasicos},
-        Gustos de largo plazo: ${gustosLargoPlazo}, Gustos de corto plazo: ${gustosCortoPlazo} y para emeregencias esta el coclchon; ${emergencias}`);
-    */
-
     //Inserto los datos en la tabla
 
-    const tabla = document.querySelector(".calculadora");
 
-
-    var tr = document.querySelector("tr");
-    tr.setAttribute("class", "table-secondary");
-    tabla.appendChild(tr);
-
-    var tr1 = document.createElement("th");
-    tr1.textContent = `-`
-    tabla.appendChild(tr1);
-
-    var th4 = document.createElement("th");
+    const th4 = document.querySelector(".td1");
     th4.textContent = `${ingreso}`;
-    tabla.appendChild(th4);
 
-    var td1 = document.createElement("th");
+
+    const td1 = document.querySelector(".td2");
     td1.textContent = `${paraTodaLaVida}`;
-    tabla.appendChild(td1);
 
-    var td2 = document.createElement("th");
+
+    const td2 = document.querySelector(".td3");
     td2.textContent = `${gastosBasicos}`;
-    tabla.appendChild(td2);
 
-    var td3 = document.createElement("th");
+    const td3 = document.querySelector(".td4");
     td3.textContent = `${gustosLargoPlazo}`;
-    tabla.appendChild(td3);
 
-    var td4 = document.createElement("th");
+    const td4 = document.querySelector(".td5");
     td4.textContent = `${gustosCortoPlazo}`;
-    tabla.appendChild(td4);
 
-    var td5 = document.createElement("th");
+    const td5 = document.querySelector(".td6");
     td5.textContent = `${emergencias}`;
-    tabla.appendChild(td5);
 
     //cancelamos el comportamiento del evento
 
     e.preventDefault();
 
-    //obtenemos el elemento desde el cual se disparo el evento
-
-    let miformulario = e.target
-
-    miformulario.reset();
 }
