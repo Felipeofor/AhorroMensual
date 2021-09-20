6822e858108c73bdb82fb21abb5af3f2dafea45b
Ramos Felipe <felipeofor@gmail.com>
2021-09-20 09:08:18 -0300

    redes, about

--- js/miarchivo.js
+++ js/miarchivo.js
@@ -4,6 +4,10 @@ const enviar = document.querySelector(".add");
 
 enviar.addEventListener("click", calcular);
 
+function limpiar() {
+    document.getElementById("floatingInput").value = "";
+}
+
 function calcular(e) {
 
 
@@ -19,13 +23,16 @@ function calcular(e) {
     let gustosLargoPlazo = sueldoReal * 0.10;
     let emergencias = sueldoReal * 0.20;
 
-    console.log(ingreso, paraTodaLaVida, sueldoReal);
+
 
     //Inserto los datos en la tabla
 
+    const th1 = document.querySelector(".th1");
+    th1.textContent = `MONTO INGRESADO:`;
 
-    // const th4 = document.querySelector(".td1");
-    // th4.textContent = `${ingreso}`;
+
+    const th4 = document.querySelector(".td1");
+    th4.textContent = `$${ingreso}`;
 
 
     const td1 = document.querySelector(".td2");
@@ -47,5 +54,5 @@ function calcular(e) {
     //cancelamos el comportamiento del evento
 
     e.preventDefault();
-
+    limpiar()
 }
